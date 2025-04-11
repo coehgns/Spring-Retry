738bc2f3e9b361f587e913a16d46da35ecb98fd6
도훈 <modooboiroo@gmail.com>
2025-04-11 22:10:08 +0900

    refactor: CreateBoardService에 Retry 설정

--- src/main/kotlin/com/example/demospringretry/domain/board/service/CreateBoardService.kt
+++ src/main/kotlin/com/example/demospringretry/domain/board/service/CreateBoardService.kt
@@ -3,6 +3,10 @@ package com.example.demospringretry.domain.board.service
 import com.example.demospringretry.domain.board.domain.Board
 import com.example.demospringretry.domain.board.domain.repository.BoardRepository
 import com.example.demospringretry.domain.board.presentation.dto.request.CreateBoardRequest
+import org.slf4j.LoggerFactory
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.Recover
+import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -10,6 +14,17 @@ import org.springframework.transaction.annotation.Transactional
 class CreateBoardService(
     private val boardRepository: BoardRepository
 ) {
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(CreateBoardService::class.java)
+    }
+
+    @Retryable(
+        maxAttempts = 3,
+        backoff = Backoff(delay = 1000),
+        include = [RuntimeException::class],
+        recover = "recover"
+    )
     @Transactional
     fun execute(request: CreateBoardRequest) {
         val board = request.run {
@@ -18,7 +33,11 @@ class CreateBoardService(
                 content = content
             )
         }
-
         boardRepository.save(board)
+    }
+
+    @Recover
+    fun recover(e: RuntimeException) {
+        logger.info("recover $e")
     }
 }
