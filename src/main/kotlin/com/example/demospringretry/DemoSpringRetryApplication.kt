c255d6087c99effd050f0733e6f24b5bb2bb0971
도훈 <modooboiroo@gmail.com>
2025-04-11 21:30:42 +0900

    feat: @EnableRetry 추가

--- src/main/kotlin/com/example/demospringretry/DemoSpringRetryApplication.kt
+++ src/main/kotlin/com/example/demospringretry/DemoSpringRetryApplication.kt
@@ -2,7 +2,10 @@ package com.example.demospringretry
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.retry.annotation.EnableRetry
+import org.springframework.retry.annotation.Retryable
 
+@EnableRetry
 @SpringBootApplication
 class DemoSpringRetryApplication
 
