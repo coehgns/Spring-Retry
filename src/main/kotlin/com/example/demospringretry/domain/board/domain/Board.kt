a6adac682b0d01d2e7f2b803c5b2f74ac4e527e5
도훈 <modooboiroo@gmail.com>
2025-04-11 21:34:48 +0900

    feat: Board Entity 추가

---
+++ src/main/kotlin/com/example/demospringretry/domain/board/domain/Board.kt
@@ -0,0 +1,20 @@
+package com.example.demospringretry.domain.board.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+class Board(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    @Column(name = "title")
+    var title: String,
+
+    @Column(name = "content")
+    var content: String? = null
+)
