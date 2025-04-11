3220d46d3fa361fb0aa521bdd28e1a200e5cc920
도훈 <modooboiroo@gmail.com>
2025-04-11 19:00:25 +0900

    first commit

---
+++ src/test/kotlin/com/example/demospringretry/DemoSpringRetryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.demospringretry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoSpringRetryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
